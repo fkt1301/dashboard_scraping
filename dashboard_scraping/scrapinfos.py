6ee6d256fb9b67f1e3c5162cb95e17655f212a48
Olivier Sansamat <olivier.sansamat@edhec.com>
2023-01-19 10:56:02 +0100

    improved get links

--- dashboard_scraping/scrapinfos.py
+++ dashboard_scraping/scrapinfos.py
@@ -1,9 +1,11 @@
 import requests
+from multiprocessing import Pool
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import csv
 from pprint import pprint
 import os
+import time
 
 def diff(list1, list2):
     """
@@ -11,21 +13,66 @@ def diff(list1, list2):
     """
     return (list(set(list1).symmetric_difference(set(list2))))
 
-def links_to_scrap():
+def get_links_to_scrap():
     """
     Define what is remaining to scrap
 
-    Uses diff function
+    Uses diff function to assess differene between already scraped and to-scrap
     """
     if os.path.isfile("data/infos.csv"): # resume scraping
-        previously_scrapped = pd.read_csv("data/scrapinfos.csv")
-        previously_scrapped_links = previously_scrapped["link"]
-        all_cities = pd.read_csv("data/liensvilles.csv")
-        all_cities_links = all_cities["link"]
-        links_to_scrap = diff(previously_scrapped_links, all_cities_links)
+        previously_scrapped = pd.read_csv("data/infos.csv") # Must have a "link" header
+        all_links = pd.read_csv("data/liensvilles.csv")
+        links_to_scrap = diff(previously_scrapped.iloc[:,0], all_links["link"])
     else: # start scraping for scratch
-        links_to_scrap = pd.read_csv("data/liensvilles.csv", nrows=200)
+        all_links = pd.read_csv("data/liensvilles.csv")
+        links_to_scrap = all_links["link"]
     return links_to_scrap
+
+# def write_headers(document):
+#     with open("data/infos.csv", "a", encoding="utf-8") as csvfile:
+#         writer = csv.writer(csvfile)
+
+
+#     headers = ["link"
+#                 #, "city"
+#                 ]
+
+#     writer.writerow(headers)
+
+
+def parse(link):
+    req = requests.get(link)
+    time.sleep(2)
+    if req.status_code == 200:
+        with open("data/infos.csv", "a", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            content = req.content
+            soup = bs(content, "html.parser")
+
+            dico = {}
+            dico["link"] = link
+            #dico["city"] = all_links[all_links["link"]==link]["city"].iloc[0]
+
+            # Get all tables for the city
+            tables = soup.findAll('table', class_='odTable odTableAuto')
+
+            # Get all tables for the city
+            for i in range(len(tables)):
+                all_tr = tables[i].findAll('tr')
+                # Get all info for each table
+                for tr in all_tr[1:]:
+                    k = tr.findAll('td')[0].text # labels
+                    v = tr.findAll('td')[1].text # data
+
+                    if "Nom des habitants" in k: # to have identical column names ; city may vary
+                        dico["Nom des habitants"] = v
+                    elif "Taux de chômage" in k: # to have identical column names ; year may vary
+                        dico["Taux de chômage"] = v
+                    else:
+                        dico[k] = v
+
+            writer.writerow(dico.values())
+            print("Wrote information to csv for link " + link)
 
 def create_info_csv(links_to_scrap):
     """
@@ -77,6 +124,8 @@ def create_info_csv(links_to_scrap):
         writer.writerows(dicos)
 
 if __name__ == "__main__":
-    to_scrap = links_to_scrap()
-    create_info_csv(to_scrap)
-    print("csv created")
+    links_to_scrap = get_links_to_scrap()
+    print("# links to scrap: ", len(links_to_scrap))
+    with Pool(30) as p:
+        p.map(parse, links_to_scrap)
+    # print("csv created")
